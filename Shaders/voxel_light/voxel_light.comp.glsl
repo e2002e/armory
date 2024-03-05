54101e82976079d831eadd8b258240914ca396d3
yvain <yvain29@gmail.com>
2024-03-05 22:13:17 +0100

    fixed light wposition in voxel_light. TODO: try to not use LOD, implement voxels bounce.

--- Shaders/voxel_light/voxel_light.comp.glsl
+++ Shaders/voxel_light/voxel_light.comp.glsl
@@ -1,7 +1,7 @@
 #version 450
 
 // layout (local_size_x = 4, local_size_y = 4, local_size_z = 4) in;
-layout (local_size_x = 64, local_size_y = 1, local_size_z = 1) in;
+layout (local_size_x = 8, local_size_y = 8, local_size_z = 8) in;
 
 #include "compiled.inc"
 #include "std/math.glsl"
@@ -42,12 +42,12 @@ void main() {
 	vec4 radiance = vec4(0.0);
 	vec4 emission = vec4(0.0);
 
-	for (int i = 0; i < 6 + DIFFUSE_CONE_COUNT; i++)
+	for (int i = 0; i < 6; i++)
 	{
 		ivec3 src = ivec3(gl_GlobalInvocationID.xyz);
 		src.x += i * res;
 		ivec3 dst = src;
-		dst.y += clipmapLevel * res;
+		//dst.y += clipmapLevel * res;
 
 		vec3 wposition = (gl_GlobalInvocationID.xyz + 0.5) / voxelgiResolution.x;
 		wposition = wposition * 2.0 - 1.0;
@@ -76,43 +76,44 @@ void main() {
 			}
 			else
 				radiance = mix(convRGBA8ToVec4(imageLoad(voxelsB, dst).r), radiance, 0.5);
-		}
 
-		float visibility;
-		vec3 lp = lightPos - wposition;
-		vec3 l;
-		if (lightType == 0) { l = lightDir; visibility = 1.0; }
-		else { l = normalize(lp); visibility = attenuate(distance(wposition, lightPos)); }
 
-		// float dotNL = max(dot(wnormal, l), 0.0);
-		// if (dotNL == 0.0) return;
+			float visibility;
+			vec3 lp = lightPos - wposition;
+			vec3 l;
+			if (lightType == 0) { l = lightDir; visibility = 1.0; }
+			else { l = normalize(lp); visibility = attenuate(distance(wposition, lightPos)); }
 
-	#ifdef _ShadowMap
-		if (lightShadow == 1) {
-			vec4 lightPosition = LVP * vec4(wposition, 1.0);
-			vec3 lPos = lightPosition.xyz / lightPosition.w;
-			visibility = texture(shadowMap, vec3(lPos.xy, lPos.z - shadowsBias)).r;
-		}
-		else if (lightShadow == 2) {
-			vec4 lightPosition = LVP * vec4(wposition, 1.0);
-			vec3 lPos = lightPosition.xyz / lightPosition.w;
-			visibility *= texture(shadowMapSpot, vec3(lPos.xy, lPos.z - shadowsBias)).r;
-		}
-		else if (lightShadow == 3) {
-			visibility *= texture(shadowMapPoint, vec4(-l, lpToDepth(lp, lightProj) - shadowsBias)).r;
-		}
-	#endif
+			// float dotNL = max(dot(wnormal, l), 0.0);
+			// if (dotNL == 0.0) return;
 
-		if (lightType == 2) {
-			float spotEffect = dot(lightDir, l);
-			if (spotEffect < spotData.x) {
-				visibility *= smoothstep(spotData.y, spotData.x, spotEffect);
+		#ifdef _ShadowMap
+			if (lightShadow == 1) {
+				vec4 lightPosition = LVP * vec4(wposition, 1.0);
+				vec3 lPos = lightPosition.xyz / lightPosition.w;
+				visibility = texture(shadowMap, vec3(lPos.xy, lPos.z - shadowsBias)).r;
 			}
+			else if (lightShadow == 2) {
+				vec4 lightPosition = LVP * vec4(wposition, 1.0);
+				vec3 lPos = lightPosition.xyz / lightPosition.w;
+				visibility *= texture(shadowMapSpot, vec3(lPos.xy, lPos.z - shadowsBias)).r;
+			}
+			else if (lightShadow == 3) {
+				visibility *= texture(shadowMapPoint, vec4(-l, lpToDepth(lp, lightProj) - shadowsBias)).r;
+			}
+		#endif
+
+			if (lightType == 2) {
+				float spotEffect = dot(lightDir, l);
+				if (spotEffect < spotData.x) {
+					visibility *= smoothstep(spotData.y, spotData.x, spotEffect);
+				}
+			}
+
+			radiance.rgb *= visibility * lightColor;// * dotNL;
+			radiance = clamp(radiance, vec4(0.0), vec4(1.0));
+
+			imageAtomicAdd(voxelsLight, ivec3(gl_GlobalInvocationID.xyz), convVec4ToRGBA8(radiance));
 		}
-
-		radiance.rgb *= visibility * lightColor;// * dotNL;
-		radiance = clamp(radiance, vec4(0.0), vec4(1.0));
-
-		imageAtomicAdd(voxelsLight, ivec3(gl_GlobalInvocationID.xyz), convVec4ToRGBA8(radiance));
 	}
 }
