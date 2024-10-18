5c61641c0779aa4adba24936a1463e755b2208b2
Yvain Douard <i20@lenovo.home>
2024-10-18 16:55:17 +0200

    test for ior == 1.0 in ssrefr shader.

--- Shaders/ssrefr_pass/ssrefr_pass.frag.glsl
+++ Shaders/ssrefr_pass/ssrefr_pass.frag.glsl
@@ -86,16 +86,10 @@ void main() {
     float opac = gr.y;
 
     float d = textureLod(gbufferD, texCoord, 0.0).r * 2.0 - 1.0;
-
-    if (opac == 1.0) {
+    if (opac == 1.0 || d == 0.0 || ior == 1.0) {
 		fragColor.rgb = textureLod(tex, texCoord, 0.0).rgb;
         return;
 	}
-    if (d == 0.0) {
-        fragColor.rgb = textureLod(tex1, texCoord, 0.0).rgb;
-        return;
-    }
-
     vec2 enc = g0.rg;
     vec3 n;
     n.z = 1.0 - abs(enc.x) - abs(enc.y);
